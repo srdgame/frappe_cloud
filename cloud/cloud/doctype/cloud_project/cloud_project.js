7e9ec0b50e4ce78d9806df0d082760de1398bbd8
Dirk Chang <dirk@kooiot.com>
2017-04-25 13:34:05 +0800

    Filter the cloud group by company

--- cloud/cloud/doctype/cloud_project/cloud_project.js
+++ cloud/cloud/doctype/cloud_project/cloud_project.js
@@ -2,6 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Cloud Project', {
+	setup: function(frm) {
+		frm.fields_dict["group"].get_query = function(doc){
+			return {
+				filters: {"company": doc.company}
+			};
+		};
+	},
 	refresh: function(frm) {
 
 	}
