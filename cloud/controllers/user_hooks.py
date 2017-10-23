24bc8a037a6f9d082229696cdc0705352c921cd8
Dirk Chang <dirk@kooiot.com>
2017-10-23 12:01:30 +0800

    adding roles in background job

--- cloud/controllers/user_hooks.py
+++ cloud/controllers/user_hooks.py
@@ -4,10 +4,18 @@
 
 from __future__ import unicode_literals
 import frappe
+import time
 
 
 def after_insert(doc, method):
+	frappe.enqueue('cloud.controllers.user_hooks.insert_user_roles', user=doc.name, sleep=3)
+
+
+def insert_user_roles(user, sleep=None):
+	if sleep:
+		time.sleep(sleep)
 	cloud_settings = frappe.get_doc('Cloud Settings', 'Cloud Settings')
+	doc = frappe.get_doc("User", user)
 	if len(cloud_settings.role_list) > 0:
 		roles = [d.role for d in cloud_settings.role_list]
 		doc.add_roles(*roles)
